881f5edab863726ffab1f5c5e13214959ccdd49f
Grant Dong 510472058 <gdon7480@uni.sydney.edu.au>
2024-10-04 02:41:57 +1000

    Added some comments regarding MJPEGOptions

--- rpicam-apps/core/mjpeg_options.hpp
+++ rpicam-apps/core/mjpeg_options.hpp
@@ -6,12 +6,27 @@
 
 #include "video_options.hpp"
 
-struct MjpegOptions : public VideoOptions
+struct MJPEGOptions : public VideoOptions
 {
-    MjpegOptions() : VideoOptions()
+    MJPEGOptions() : VideoOptions()
     {
         using namespace boost::program_options;
 
+        /*
+        Here we are adding in the options that were in the original RaspiMJPEG app.
+
+        Note, the following features are no longer supported:
+        Look here: https://www.raspberrypi.com/documentation/computers/camera_software.html#differences-between-rpicam-and-raspicam 
+        - opacity (--opacity)
+        - image effects (--imxfx)
+        - colour effects (--colfx)
+        - annotation (--annotate, --annotateex)
+        - dynamic range compression, or DRC (--drc)
+        - stereo (--stereo, --decimate and --3dswap)
+        - image stabilisation (--vstab)
+        - demo modes (--demo)
+        - iso (--ISO)
+        */
         options_.add_options()
         ("output-mjpeg,om",value<std::string>(&output_mjpeg)->default_value(0),
             "Set the output name for the MJPEG stream. Can be udp/tcp for network stream");
@@ -19,6 +34,7 @@ struct MjpegOptions : public VideoOptions
             "Set the output name for the low resolution preview stream. Can be udp/tcp for network stream");
         ("output-video,ov",value<std::string>(&output_video)->default_value(0),
             "Set the output name for the video stream. Can be udp/tcp for network stream");
+        // ("sharpness,sh",value<);
     }
 
     std::string output_mjpeg;
