bfe63a3709062c9390cd97a795e211cb2e623e52
Grant Dong 510472058 <gdon7480@uni.sydney.edu.au>
2024-10-16 16:56:43 +1100

    mjpeg_options.hpp has correct formatting for image, preview and video naming and paths

--- rpicam-apps/core/mjpeg_options.hpp
+++ rpicam-apps/core/mjpeg_options.hpp
@@ -32,23 +32,27 @@ struct MJPEGOptions : public VideoOptions
         - iso (--ISO)
         */
         options_.add_options()
-        ("output-mjpeg,om",value<std::string>(&output_mjpeg),
+        ("output-image,om",value<std::string>(&output_image),
             "Set the output path for the MJPEG stream. Can be udp/tcp for network stream")
         ("output-preview,op",value<std::string>(&output_preview),
             "Set the output path for the low resolution preview stream. Can be udp/tcp for network stream")
         ("output-video,ov",value<std::string>(&output_video),
             "Set the output path for the video stream. Can be udp/tcp for network stream")
         ("raw-mode,rm",value<std::string>(&raw_mode_string),
-			"Raw mode as W:H:bit-depth:packing, where packing is P (packed) or U (unpacked)");
-        // ("sharpness,sh",value<);
-
+			"Raw mode as W:H:bit-depth:packing, where packing is P (packed) or U (unpacked)")
+        ("image-count,ic",value<unsigned int>(&image_count),
+            "Set the image number for filename")
+        ("video-count,vc",value<unsigned int>(&video_count),
+            "Set the video number for filename")
         ;
     }
 
-    std::string output_mjpeg;
+    std::string output_image;
     std::string output_preview;
     std::string output_video;
     std::string raw_mode_string;
+    unsigned int image_count = 0;
+    unsigned int video_count = 0;
 
 
     /*
@@ -57,32 +61,71 @@ struct MJPEGOptions : public VideoOptions
     preview_path /dev/shm/mjpeg/cam.jpg
     image_path /var/www/media/im_%i_%Y%M%D_%h%m%s.jpg
     video_path /var/www/media/vi_%v_%Y%M%D_%h%m%s.mp4
+
      */
 
     virtual bool Parse(int argc, char *argv[]) override
     {
 
         // Testing
-        std::cout << "==================================================" << std::endl;
+        // std::cout << "==================================================" << std::endl;
 
         if (output_preview.empty())
-            output_preview = "/dev/shm/mjpeg/cam.jpg";
-        if (output_mjpeg.empty())
-            output_mjpeg = "test.mjpeg";
-        if (output_video.empty())
             {
-                // Get current time and local time
+                output_preview = "/dev/shm/mjpeg/cam.jpg";
+                // Testing
+                std::cout << output_preview << std::endl;
+            }
+            
+        if (output_image.empty())
+            {
                 std::time_t t = std::time(nullptr);
                 std::tm* now = std::localtime(&t);
-                // char buffer[100];
-                char* buffer = (char*) malloc(sizeof(char)*sizeof("/var/www/media/vi_%Y%m%d_%H%M%S.jpg"));
-                std::strftime(buffer, sizeof(buffer), "/var/www/media/vi_%Y%m%d_%H%M%S.jpg", now);
-                std::cout << buffer << std::endl;
+
+                std::string sourceString = "/var/www/media/im_" + std::to_string(image_count) + "_%Y%m%d_%H%M%S.jpg";
+                std::string bufferString = "/var/www/media/im__YYYYMMDD_HHMMSS.jpg";
+                size_t image_buffer_size = bufferString.size()+std::to_string(image_count).length()+1;
+
+                char* image_buffer = (char*)malloc(image_buffer_size);
+                if (image_buffer == nullptr) {
+                    std::cerr << "Memory allocation failed!" << std::endl;
+                    return 1;
+                }
+
+                std::strftime(image_buffer, image_buffer_size, sourceString.c_str(), now);
+
+                // Testing
+                std::cout << image_buffer << std::endl;
+
+                free(image_buffer);
+            }
+
+        if (output_video.empty())
+            {
+                std::time_t t = std::time(nullptr);
+                std::tm* now = std::localtime(&t);
+
+                std::string sourceString = "/var/www/media/vi_" + std::to_string(video_count) + "_%Y%m%d_%H%M%S.mp4";
+                std::string bufferString = "/var/www/media/vi__YYYYMMDD_HHMMSS.jpg";
+                size_t video_buffer_size = bufferString.size()+std::to_string(video_count).length()+1;
+                
+                char* video_buffer = (char*)malloc(video_buffer_size);
+                if (video_buffer == nullptr) {
+                    std::cerr << "Memory allocation failed!" << std::endl;
+                    return 1;
+                }
+                
+                std::strftime(video_buffer, video_buffer_size, sourceString.c_str(), now);
+
+                // Testing
+                std::cout << video_buffer << std::endl;
+
+                free(video_buffer);
             }
         // raw_mode = Mode(raw_mode_string);
 
 
-        std::cout << "==================================================" << std::endl;
+        // std::cout << "==================================================" << std::endl;
 
         return true;
     }
@@ -90,7 +133,7 @@ struct MJPEGOptions : public VideoOptions
     virtual void Print() const override
     {
         VideoOptions::Print();
-        std::cerr << "    output-mjpeg: " << output_mjpeg << std::endl;
+        std::cerr << "    output-mjpeg: " << output_image << std::endl;
         std::cerr << "    output-preview: " << output_preview << std::endl;
         std::cerr << "    output-video: " << output_video << std::endl;
     }
